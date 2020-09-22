0f55f3d5db20b85ea092c7d8a7dde2e9f47ffe81
rubensojunior <rubensojunior6@gmail.com>
2020-09-22 16:28:06 -0300

    delete not necessary files

---
+++ app/public/wp-content/themes/rest/footer.php
@@ -0,0 +1,17 @@
+<footer>
+			<nav class="nav-footer">
+				<ul>
+					<li class="current_page_item"><a href="index.html">Menu</a></li>
+					<li><a href="sobre.html">Sobre</a></li>
+					<li><a href="contato.html">Contato</a></li>
+				</ul>
+			</nav>
+
+			<p>Rest © 2015. Alguns direitos reservados.</p>
+		</footer>
+
+        <!-- Footer wordpress -->
+        <?php wp_footer(); ?>
+    <!-- Footer wordpress -->
+	</body>
+</html>
